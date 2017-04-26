792e3a10ae2836a4b14da7d954c38be58784a4fe
Shlok Datye <shlok@quant.is>
2017-04-26 09:51:09 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,46 @@
+# obtain_options
+
+Created by [shlok](https://github.com/shlok) for use in personal projects. Use at your own risk.
+
+``obtain_options`` is a Bash function for obtaining command line options.
+
+## Requirements
+
+Bash 4 or later.
+
+Installation on macOS: (1) `brew install bash`. (2) In Terminal → Preferences → General, change shell path to `/usr/local/bin/bash`. (3) In Terminal → Preferences → Profiles → Shell, add `-bash` to the table view. (4) Confirm that `/usr/local/bin` appears before `/bin` in `$PATH`.
+
+## Usage
+
+Example:
+
+    #!/usr/bin/env bash
+    
+    value_options=("--file" "-c" "--format")
+    one_letter_count_options=("a" "v")
+    multi_letter_count_options=("archive" "verbose")
+    
+    declare -A values
+    declare -A counts
+    non_option_arguments=()
+    
+    . ./obtain_options.bash
+    obtain_options "$@"
+    if [[ $? > 0 ]]; then
+        echo "Usage: ..."
+        exit 1
+    fi
+    
+    declare -p values
+    declare -p counts
+    declare -p non_option_arguments
+
+Running this script with the arguments
+
+    --file foo.txt -c=5 --format="a b c" -avv --archive --verbose -- other args
+
+gives us this result:
+
+    declare -A values=([-c]="5" [--file]="foo.txt" [--format]="a b c" )
+    declare -A counts=([archive]="1" [verbose]="1" [a]="1" [v]="2" )
+    declare -a non_option_arguments=([0]="other" [1]="args")
---
+++ obtain_options.bash
@@ -0,0 +1,96 @@
+# http://stackoverflow.com/a/8574392
+__element_in() {
+    local e
+    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+    return 1
+}
+
+
+obtain_options() {
+    local args=("$@")
+    local count=${#args[@]}
+    
+    # No more arguments to process. We are done.
+    if [[ count == 0 ]]; then
+        return 0
+    fi
+    
+    local fst=${args[0]}
+    local snd=${args[1]}
+    
+    # -- marks the end of options.
+    if [[ $fst = -- ]]; then
+        non_option_arguments+=("${args[@]:1}")
+        return 0
+    fi
+    
+    if __element_in $fst "${value_options[@]}"; then
+        # $fst examples:
+        #     "--file" (where "--file" is in value_options)
+        #     "-c" (where "-c" is in value_options)
+        
+        if [[ -z $snd ]]; then
+            return 1
+        else
+            # $snd is a valid value.
+            values[$fst]=$snd
+            obtain_options "${args[@]:2}"
+            return $?
+        fi
+    elif [[ $fst =~ ^(--.+)=(.*)$ ]] || [[ $fst =~ ^(-.)=(.*)$ ]]; then
+        # $fst examples:
+        #     "--file=foo.txt"
+        #     "-c=something"
+        #     "--file="
+        #     "-c="
+        
+        local option=${BASH_REMATCH[1]}
+        local value=${BASH_REMATCH[2]}
+        
+        if __element_in $option "${value_options[@]}"; then
+            # $option, e.g., "--file" or "-c", is in value_options.
+            if [[ -n $value ]]; then values[$option]=$value; fi
+            obtain_options "${args[@]:1}"
+            return $?
+        else
+            return 1
+        fi
+    elif [[ $fst =~ ^--(.+)$ ]]; then
+        # $fst example:
+        #     "--archive"
+        
+        local option=${BASH_REMATCH[1]}
+        
+        if __element_in $option "${multi_letter_count_options[@]}"; then
+            (( counts[$option]+=1 ))
+            obtain_options "${args[@]:1}"
+            return $?
+        else
+            return 1
+        fi
+    elif [[ $fst = -?* ]]; then
+        # $fst examples:
+        #     "-a"
+        #     "-avz"
+        
+        local i=1
+        for (( i=1; i<${#fst}; i++ )); do
+            local letter="${fst:$i:1}"            
+            if __element_in $letter "${one_letter_count_options[@]}"; then
+                (( counts[$letter]+=1 ))
+            else
+                return 1
+            fi
+        done
+        
+        obtain_options "${args[@]:1}"
+        return $?
+    else
+        # Options are done.
+        non_option_arguments+=("${args[@]}")
+        return 0
+    fi
+    
+    # Impossible.
+    return 1
+}
